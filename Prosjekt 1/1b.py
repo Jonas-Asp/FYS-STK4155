d09340e4879811cba614911d356b0c7424d58d12
Jonas <jonasasp@student.matnat.uio.no>
2018-09-25 11:52:29 +0200

    Satt alt i funksjoner

--- Prosjekt 1/1b.py
+++ Prosjekt 1/1b.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 24 14:21:47 2018
+Created on Tue Sep 25 10:33:32 2018
 
-@author: Rafiki
+@author: Jonas Asperud
 """
 
 from mpl_toolkits.mplot3d import Axes3D
@@ -14,22 +14,30 @@ import numpy as np
 from random import random, seed
 from scipy import linalg
 from sklearn.metrics import mean_squared_error,r2_score
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-# Make data.
-x = np.arange(0, 1, 0.05)
-y = np.arange(0, 1, 0.05)
-x, y = np.meshgrid(x,y)
+from sklearn.preprocessing import PolynomialFeatures
 
  # Bootstrap
-def bootstrap(self, nBoots = 1):
-    bootVec = np.zeros(nBoots)
-    for k in range(0,nBoots):
-        print(np.random.choice(self.data, len(self.data)))
+def bootstrap(sampleData,nBoots,designMatrix):
+    # Initiate matrices
+    bootVec = np.zeros(len(sampleData))
+    b = np.zeros((nBoots,designMatrix.shape[1]))
+    mse = np.zeros(nBoots)
+    r2 = np.zeros(nBoots)
     
+    # Loops through the set number of boots
+    for k in range(0,nBoots):
+        # Chooses a random set of data points from data, with same number of rows as data
+        bootVec = np.random.choice(sampleData, len(sampleData))
+        # Calculates beta values
+        b[k,:] = linalg.inv(designMatrix.T.dot(designMatrix)).dot(designMatrix.T).dot(bootVec)
+        # Create a fit model for the data
+        bootpred = designMatrix.dot(b[k,:]).flatten()
+        # Does error calculation
+        mse[k] = mean_squared_error(sampleData,bootpred)
+        r2[k] = r2_score(sampleData,bootpred)
+    return mse,r2,b
 
+# Creating the FrankeFunction
 def FrankeFunction(x,y):
     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
@@ -37,76 +45,54 @@ def FrankeFunction(x,y):
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
+#Bare for ha en enkel plotte funksjon
+def plotting(x,y,z):
+    # Plot the surface.
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x,y,z,cmap=plt.cm.viridis,linewidth=0, antialiased=False)
+    # Customize the z axis.
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()    
+
+# Make data.
+x = np.arange(0, 1, 0.05)
+y = np.arange(0, 1, 0.05)
+x, y = np.meshgrid(x,y)
 z = FrankeFunction(x,y)
-z1 = z.flatten()
-x1 = x.flatten()
-y1 = y.flatten()
 
-lam = 0.01
+# Setting the right format for the matrices
+Z = z.flatten()
+X = x.flatten()
+Y = y.flatten()
 
-# Finner beta verdier
-xy = np.c_[np.ones((z.size,1)),x1,y1
-           ,x1**2,x1*y1,y1**2
-           ,x1**3,x1**2*y1,x1*y1**2,y1**3
-           ,x1**4,x1**3*y1,x1**2*y1**2,x1*y1**3,y1**4
-           ,x1**5,x1**4*y1,x1**3*y1**2,x1**2*y1**3,x1*y1**4,y1**5]
+# Creating design matrix
+XY = np.c_[X,Y]
+degree = PolynomialFeatures(degree=5)
+dMatrix = degree.fit_transform(XY)
 
-beta = linalg.inv(xy.T.dot(xy) - lam*np.identity(len(z)+1)).dot(xy.T).dot(z1)
+# Calculates beta values and predict the function using this fit/model
+beta = linalg.inv(dMatrix.T.dot(dMatrix)).dot(dMatrix.T).dot(Z)
 
-zpred = xy.dot(beta).flatten()
+# Bootstrapping (The real matrix, Number of boots, design-matrix)
+bootMSE, bootR2, bootBeta = bootstrap(Z,1,dMatrix)
 
 
-# The beta values as polynomial
-print("######## Beta verdier i stigende rekkefølge ###########")
-print(beta)      
-print("")
-
-# Ser på error
-mse = mean_squared_error(z1,zpred)
-print("######## Error vurdering ###########")
-print("MSE = %.4f" %mse)
-print("R2 = %.4f" %r2_score(z1,zpred)) 
-print("") 
-
-# Finner variansen som funksjon av covariance matrisen
-sigma = mse*(100/(100-5-1))
-var = linalg.inv(xy.T.dot(xy)).dot(sigma)
-print("######## Variansen som diagonal elementer i covarians matrisen ###########")
-print(np.diagonal(var))
-print("")  
-
-# Confidense interval ?
-print("######## Confidence interval som 2*sqrt(diagonal(var)) ??? ###########")
-print(2*np.sqrt(np.diagonal(var)))
+# Prediction and plotting
+zpred = dMatrix.dot(bootBeta.flatten()).flatten()
+zpred = zpred.reshape(20,20)
+plotting(x,y,z)
+plotting(x,y,zpred)
 
 
 
 
 
 
-############### Plotting
-# Plot the surface.
-surf = ax.plot_surface(x,y,z,cmap=plt.cm.viridis,linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-
-plt.show()
 
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-#Reshape
-zpredplot = zpred.reshape(20,20)
-surf2 = ax.plot_surface(x,y,zpredplot,cmap=plt.cm.viridis,linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-plt.show()
+
